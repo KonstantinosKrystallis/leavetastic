b60ea8676dac16680c401444f22465ab6e0de6a5
Konstantinos Krystallis <konkry1@hotmail.com>
2023-01-21 13:56:08 +0200

    Created base test page

---
+++ LeaveTastic/Client/Pages/FetchData.razor.cs
@@ -0,0 +1,29 @@
+﻿using LeaveTastic.Common.Services;
+using LeaveTastic.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
+
+namespace LeaveTastic.Client.Pages
+{
+    public partial class FetchData : ComponentBase
+    {
+        [Inject]
+        private HelperService helperService { get; set; } = default!;
+
+        private IEnumerable<WeatherForecast>? forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                forecasts = await helperService.HttpService.Get<List<WeatherForecast>>("WeatherForecast");
+            }
+            catch (AccessTokenNotAvailableException exception)
+            {
+                exception.Redirect();
+            }
+        }
+    }
+}
